c91758e1c4f0f7d1790f1efe1f7372555be8a5f1
Rony <rocaakanne@gmail.com>
2025-12-03 12:47:38 -0400

    Se arregló el sonido

--- app/src/main/java/com/example/proyectoandroid/Sonidos.java
+++ app/src/main/java/com/example/proyectoandroid/Sonidos.java
@@ -4,50 +4,55 @@ import android.content.Context;
 import android.media.MediaPlayer;
 
 public class Sonidos {
-    public static void reproducir (Context context, String nombre){
-        MediaPlayer sonido;
-        switch (nombre){
-            case "bien":{
-                sonido = MediaPlayer.create(context, R.raw.bien);
-                break;
-            }
-            case "okis":{
-                sonido = MediaPlayer.create(context, R.raw.okis);
-                break;
-            }
-            case "no":{
-                sonido = MediaPlayer.create(context, R.raw.no_okis);
-                break;
-            }
-            case "clic":{
-                sonido = MediaPlayer.create(context, R.raw.ganaste);
-                break;
-            }
-            case "atras":{
-                sonido = MediaPlayer.create(context, R.raw.ok);
-                break;
-            }
-            case "datos":{
-                sonido = MediaPlayer.create(context, R.raw.ganaste_a);
-                break;
-            }
-            case "ronda":{
-                sonido = MediaPlayer.create(context, R.raw.interface_positive);
-                break;
-            }
-            case "victoria":{
-                sonido = MediaPlayer.create(context, R.raw.level_complete);
-                break;
-            }
-            case "calc":{
-                sonido = MediaPlayer.create(context, R.raw.otro);
-                break;
-            }
-            default:
-                sonido = MediaPlayer.create(context, R.raw.no_okis);
-                break;
+    private static MediaPlayer sonido;
 
+    public static void reproducir(Context context, String nombre) {
+        if (sonido != null) {
+            sonido.release();
+            sonido = null;
         }
+
+        int resId;
+
+        switch (nombre) {
+            case "bien":
+                resId = R.raw.bien;
+                break;
+            case "okis":
+                resId = R.raw.okis;
+                break;
+            case "no":
+                resId = R.raw.no_okis;
+                break;
+            case "clic":
+                resId = R.raw.ganaste;
+                break;
+            case "atras":
+                resId = R.raw.ok;
+                break;
+            case "datos":
+                resId = R.raw.ganaste_a;
+                break;
+            case "ronda":
+                resId = R.raw.interface_positive;
+                break;
+            case "victoria":
+                resId = R.raw.level_complete;
+                break;
+            case "calc":
+                resId = R.raw.otro;
+                break;
+            default:
+                resId = R.raw.no_okis;
+        }
+
+        sonido = MediaPlayer.create(context, resId);
+        sonido.setOnCompletionListener(mp -> {
+            mp.release();
+        });
+
         sonido.start();
     }
 }
+
+
